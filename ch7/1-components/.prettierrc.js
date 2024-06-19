804df4149de0604807ce1f92c474238b6c4d202a
nahaf25HomePC <nahaf25@gmail.com>
2024-06-19 15:16:24 +0800

    新增套件設定檔(Prettier - Code formatter)

---
+++ ch7/1-components/.prettierrc.js
@@ -0,0 +1,14 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2, //指定每個縮排等級的空格數。
+  semi: false, //在語句末尾列印分號。
+  singleQuote: true, //使用單引號而不是雙引號。
+}
+
+export default config
