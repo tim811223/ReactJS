b16bcfa2a7bcff255aaba3ab8668e66e8806e05c
nahaf25HomePC <nahaf25@gmail.com>
2024-06-19 16:23:40 +0800

    ch6-64   Class Component 寫法介紹

--- ch7/1-components/src/App.tsx
+++ ch7/1-components/src/App.tsx
@@ -1,8 +1,10 @@
 import { Anya } from './Anya';
+import { ClassAnay } from './ClassComponent';
 
 export const App = () => {
   return (
     <>
+      <ClassAnay />
       <Anya />
     </>
   );
---
+++ ch7/1-components/src/ClassComponent.tsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export class ClassAnay extends Component {
+  render() {
+    return <>Class Compenent 教學</>;
+  }
+}
