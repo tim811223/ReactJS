1e8cef082b72d55dffef8e529a54e2b23985266a
nahaf25HomePC <nahaf25@gmail.com>
2024-06-23 18:18:52 +0800

    ch8-77  使用react hook的useStake控制state,讓畫面rerender

--- ch8/1-InteractWithUser/src/ThumbsUp.tsx
+++ ch8/1-InteractWithUser/src/ThumbsUp.tsx
@@ -1,10 +1,11 @@
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export const ThumbsUp = () => {
-  let likes = 0;
+  const [likes, setlikes] = useState<number>(0);
   return (
-    <button className="button is-primary" onClick={() => likes++}>
+    <button className="button is-primary" onClick={() => setlikes(likes + 1)}>
       <span className="icon">
         <FontAwesomeIcon icon={faThumbsUp} />
       </span>
