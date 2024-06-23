02340e14a75fc944638c50dc0d9153d816b2e3e8
nahaf25HomePC <nahaf25@gmail.com>
2024-06-23 17:16:34 +0800

    ch8-74  加上 event 當 click 新增 likes

--- ch8/1-InteractWithUser/src/ThumbsUp.tsx
+++ ch8/1-InteractWithUser/src/ThumbsUp.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const ThumbsUp = () => {
   let likes = 0;
   return (
-    <button className="button is-primary">
+    <button className="button is-primary" onClick={() => likes++}>
       <span className="icon">
         <FontAwesomeIcon icon={faThumbsUp} />
       </span>
