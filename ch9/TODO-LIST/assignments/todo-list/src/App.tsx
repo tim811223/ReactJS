314fb213d1c929b799acf6fafad33515219816f5
nahaf25HomePC <nahaf25@gmail.com>
2024-06-24 17:39:57 +0800

    ch9-85  新增表單的編輯畫面

--- ch9/TODO-LIST/assignments/todo-list/src/App.tsx
+++ ch9/TODO-LIST/assignments/todo-list/src/App.tsx
@@ -1,9 +1,12 @@
+import { Editor } from "./Todos/Editor";
 import { TodoItem, Priority } from "./Todos/TodoItem";
 import { Todolist } from "./Todos/Todolist";
 
 export const App = () => {
   return (
     <>
+      <Editor />
+
       <TodoItem
         title={"title"}
         content={"content"}
