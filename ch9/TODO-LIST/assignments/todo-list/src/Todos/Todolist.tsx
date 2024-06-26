d79d67ca20505894dbcf1fa4399d50808c509be5
nahaf25HomePC <nahaf25@gmail.com>
2024-06-26 15:53:58 +0800

    ch9-96 把getTodoItems(10)搬出去外面,以免每次呼叫Todolist都產生新的資料,這樣跟本沒法測試修改功能

--- ch9/TODO-LIST/assignments/todo-list/src/Todos/Todolist.tsx
+++ ch9/TODO-LIST/assignments/todo-list/src/Todos/Todolist.tsx
@@ -2,8 +2,9 @@ import { FC } from "react";
 import { TodoItem } from "./TodoItem";
 import { getTodoItems } from "./utils/getTodoItems";
 
+const Items = getTodoItems(10);
+
 export const Todolist: FC = () => {
-  const Items = getTodoItems(10);
   const todoItems = Items.map((i) => <TodoItem {...i} />);
 
   return <>{todoItems}</>;
