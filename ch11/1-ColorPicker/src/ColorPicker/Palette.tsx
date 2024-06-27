9d4b03680f9bae818713b28d0f6de784fe50cf25
nahaf25HomePC <nahaf25@gmail.com>
2024-06-27 16:50:32 +0800

    ch11-106  完成顏色選取與抓取顏色色碼

--- ch11/1-ColorPicker/src/ColorPicker/Palette.tsx
+++ ch11/1-ColorPicker/src/ColorPicker/Palette.tsx
@@ -1,16 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 interface Props {
   colors: string[];
 }
 
 export const Palette: FC<Props> = ({ colors }) => {
+  const [val, setVal] = useState<number>(0);
+
   return (
     <>
       <div className="palette">
         {colors.map((c, i) => (
-          <div style={{ backgroundColor: c }} className="cell" key={c}>
-            <span className="val">{i}</span>
+          <div
+            style={{ backgroundColor: c }}
+            className="cell"
+            key={c}
+            onClick={() => setVal(i)}
+            onMouseEnter={(e) => e.button & 1 && setVal(i)}
+          >
+            <span className="val" hidden={i !== val}>
+              {i}
+            </span>
           </div>
         ))}
       </div>
