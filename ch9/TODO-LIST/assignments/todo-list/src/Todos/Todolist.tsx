3a7b6d80e69ba76c49471fc296876c10015cb4fb
nahaf25HomePC <nahaf25@gmail.com>
2024-06-27 09:23:55 +0800

    ch9-96  完成更新的function

--- ch9/TODO-LIST/assignments/todo-list/src/Todos/Todolist.tsx
+++ ch9/TODO-LIST/assignments/todo-list/src/Todos/Todolist.tsx
@@ -5,8 +5,11 @@ import { TodoItemModel, getTodoItems } from "./utils/getTodoItems";
 const Items = getTodoItems(10);
 
 export const Todolist: FC = () => {
-  const [todos, setTodo] = useState<TodoItemModel[]>(Items);
-  const updateTodo = (id: string, update: Partial<TodoItemModel>) => {};
+  const [todos, setTodos] = useState<TodoItemModel[]>(Items);
+  const updateTodo = (id: string, update: Partial<TodoItemModel>) => {
+    /** { ...i, ...update } 這表示原來的資料跟更新的資料合併, 若重複的內容update會蓋掉原來的資料 **/
+    setTodos(todos.map((i) => (i.id === id ? { ...i, ...update } : i)));
+  };
 
   return (
     <>
