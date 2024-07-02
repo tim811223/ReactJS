70d5c625e58424a039b4ff3e0cf593be727e6c98
nahaf25HomePC <nahaf25@gmail.com>
2024-07-02 15:58:52 +0800

    ch11-108  調色盤加上調色後的結果顯示區塊

--- ch11/1-ColorPicker/src/ColorPicker/ColorPicker.tsx
+++ ch11/1-ColorPicker/src/ColorPicker/ColorPicker.tsx
@@ -1,13 +1,19 @@
 import "./ColorPicker.scss";
 import { FC } from "react";
 import { HSL } from "./HSL";
+import "bulma/css/bulma.min.css";
 
 export const ColorPicker: FC = () => {
   return (
     <>
-      <div>
-        <div className="color-picker">
-          <HSL />
+      <div className="color-picker">
+        <div className="columns is-mobile">
+          <div className="column is-3">
+            <div className="preview">COLOR PREVIEW</div>
+          </div>
+          <div className="column">
+            <HSL />
+          </div>
         </div>
       </div>
     </>
