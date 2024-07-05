06f272bed738659aba499ac71636717fc04271aa
nahaf25HomePC <nahaf25@gmail.com>
2024-07-05 18:30:53 +0800

    ch11-112 useMemo()使用

--- ch11/1-ColorPicker/src/ColorPicker/H.tsx
+++ ch11/1-ColorPicker/src/ColorPicker/H.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { Palette } from "./Palette";
 import _ from "lodash";
 import { hsl } from "./util";
@@ -9,13 +9,14 @@ interface Props {
 }
 
 export const H: FC<Props> = ({ initVal, onSelect }) => {
+  /**
+   * 如果 dependencies 没有发生变化，React 将直接返回相同值
+   */
+  const colors = useMemo(() => _.range(360).map((h) => hsl(h, 100, 50)), []);
+
   return (
     <>
-      <Palette
-        initVal={initVal}
-        colors={_.range(360).map((h) => hsl(h, 100, 50))}
-        onSelect={onSelect}
-      />
+      <Palette initVal={initVal} colors={colors} onSelect={onSelect} />
     </>
   );
 };
