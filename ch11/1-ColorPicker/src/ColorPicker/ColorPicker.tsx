764bc0ccbad20f18ac3f8719e2676ee0d29abe50
nahaf25HomePC <nahaf25@gmail.com>
2024-07-05 17:36:41 +0800

    ch11-108 完成畫布區塊

--- ch11/1-ColorPicker/src/ColorPicker/ColorPicker.tsx
+++ ch11/1-ColorPicker/src/ColorPicker/ColorPicker.tsx
@@ -1,19 +1,31 @@
 import "./ColorPicker.scss";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { HSL } from "./HSL";
 import "bulma/css/bulma.min.css";
-import cv from 'color-convert';
+import cv from "color-convert";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
 
 export const ColorPicker: FC = () => {
+  const [rgb, setRgb] = useState<[number, number, number]>([255, 0, 0]);
+  const rgbstr = `#${cv.rgb.hex(rgb)}`;
+  const [r, g, b] = rgb;
+  const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
+
   return (
     <>
       <div className="color-picker">
         <div className="columns is-mobile">
           <div className="column is-3">
-            <div className="preview">COLOR PREVIEW</div>
+            <div className="preview" style={{ backgroundColor: rgbstr }}>
+              <p style={{ color: luminance < 128 ? "white" : "black" }}>
+                {rgbstr}
+              </p>
+              <FontAwesomeIcon icon={faCopy} />
+            </div>
           </div>
           <div className="column">
-            <HSL />
+            <HSL onSelect={(h, s, l) => setRgb(cv.hsl.rgb([h, s, l]))} />
           </div>
         </div>
       </div>
--- ch11/1-ColorPicker/src/ColorPicker/HSL.tsx
+++ ch11/1-ColorPicker/src/ColorPicker/HSL.tsx
@@ -4,19 +4,36 @@ import { H } from "./H";
 import { S } from "./S";
 import { L } from "./L";
 
-export const HSL: FC = () => {
+interface Props {
+  onSelect: (h: number, s: number, l: number) => void;
+}
+
+export const HSL: FC<Props> = ({ onSelect }) => {
   const [initH, initS, initL] = [0, 100, 50];
   const [h, setH] = useState<number>(initH);
   const [s, setS] = useState<number>(initS);
   const [l, setL] = useState<number>(initL);
 
+  const handleHSelect = (h: number) => {
+    setH(h);
+    onSelect(h, s, l);
+  };
+  const handleSSelect = (s: number) => {
+    setS(s);
+    onSelect(h, s, l);
+  };
+  const handleLSelect = (l: number) => {
+    setL(l);
+    onSelect(h, s, l);
+  };
+
   return (
     <>
       <div>
         <div className="color-picker">
-          <H initVal={initH} onSelect={setH} />
-          <S initVal={initS} hue={h} onSelect={setS} />
-          <L initVal={initL} hue={h} onSelect={setL} />
+          <H initVal={initH} onSelect={handleHSelect} />
+          <S initVal={initS} hue={h} onSelect={handleSSelect} />
+          <L initVal={initL} hue={h} onSelect={handleLSelect} />
         </div>
       </div>
     </>
