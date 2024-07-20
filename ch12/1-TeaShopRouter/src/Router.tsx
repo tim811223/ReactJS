a6af3321ae4a5b37475dff3bbb62b48b852bf059
nahaf25HomePC <nahaf25@gmail.com>
2024-07-20 16:08:59 +0800

    ch12-117 加入層級是router,根據url與router的設定一層一層render,因此order跟receipt都會在<div className="TeaShop">內

--- ch12/1-TeaShopRouter/src/Router.tsx
+++ ch12/1-TeaShopRouter/src/Router.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TeaShop } from "./Tea-Shop/TeaShop";
 import { Receipt } from "./Tea-Shop/Receipt";
 import { _Order } from "./FakeData/Data";
+import { Order } from "./Tea-Shop/Order";
 
 export const Router: FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="order" element={<TeaShop isPlaced={false} />}></Route>
-          <Route path="receipt" element={<Receipt order={_Order} />}></Route>
+          <Route path="/" element={<TeaShop />}>
+            <Route path="order" element={<Order isPlaced={false} />}></Route>
+            <Route path="receipt" element={<Receipt order={_Order} />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ ch12/1-TeaShopRouter/src/Tea-Shop/Order.tsx
@@ -0,0 +1,109 @@
+import "/public/TeaShop.scss";
+import { Form } from "./Form";
+import { OrderList } from "./OrderList";
+import { Item } from "../models/Item";
+import { ShoppingCart } from "../models/ShoppingCart";
+import { Tea, TeaPrice } from "../models/enum/Tea";
+import { Size } from "../models/enum/Size";
+import { FC, useState } from "react";
+import { v4 as uuid } from "uuid";
+import { _item, _Order, _ShoppingCart } from "../FakeData/Data";
+import { Receipt } from "./Receipt";
+import { Order as OrderModels } from "../models/Order";
+
+interface Props {
+  isPlaced: boolean;
+  // showOrder:(_isPlaced:boolean)=> void;
+}
+
+/**
+ * 最外層Componment
+ */
+export const Order: FC<Props> = (prop) => {
+  /*************
+   * Hook Area
+   * ***********/
+
+  /*************
+   * State Area
+   * ***********/
+  const [ShoppingCarts, setShoppingCarts] =
+    useState<ShoppingCart[]>(_ShoppingCart);
+  const [order, setOrder] = useState<OrderModels>({} as OrderModels);
+  const [orders, setOrders] = useState<OrderModels[]>([]);
+  const [isPlaced, setisPlaced] = useState<boolean>(prop.isPlaced);
+
+  /*************
+   * Todo Area
+   * ***********/
+  const ItemPrice = (item: Item) => {
+    const price = Object.entries(Tea)
+      .filter(([_k, _v]) => isNaN(_k as any) && _v === item["tea"])
+      .map(
+        ([_k, _v]) =>
+          parseInt(TeaPrice[_k as keyof typeof Tea].toString()) *
+          parseInt(item["quantity"].toString()),
+      )[0];
+    return price;
+  };
+  const addShoppingCart = (newItem: ShoppingCart) => {
+    setShoppingCarts([...ShoppingCarts, newItem]);
+  };
+  const deleteShoppingCart = (id: string) => {
+    setShoppingCarts(ShoppingCarts.filter((i) => i.id !== id));
+  };
+  const addOrders = (_order: OrderModels) => {
+    setOrders([...orders, _order]);
+    setOrder(_order);
+  };
+  const changeisPlaced = () => {
+    setisPlaced(!isPlaced);
+  };
+
+  /*************
+   * Handler Area
+   * ***********/
+  const handleCreateOrder = (ShoppingCart: ShoppingCart[]) => {
+    const id = Math.floor(Math.random() * 9999).toString();
+    let _TotalPrice = 0;
+    ShoppingCart?.forEach((item) => {
+      _TotalPrice += item.price;
+    });
+
+    addOrders({
+      id: id,
+      TotalPrice: _TotalPrice,
+      itemShoppingCart: ShoppingCart,
+    });
+
+    changeisPlaced();
+
+    // 透過pushState改變網址
+    window.history.pushState(null, "", "/receipt/" + id);
+  };
+  const handleAddShoppingCart = (item: Item) => {
+    const totalPrice = ItemPrice(item);
+    addShoppingCart({ id: uuid(), price: totalPrice, ...item });
+  };
+  const handleDeleteShoppingCart = (id: string) => {
+    deleteShoppingCart(id);
+  };
+
+  return (
+    <>
+      <div hidden={isPlaced}>
+        <Form item={_item} onSave={handleAddShoppingCart} />
+        <hr />
+        <OrderList
+          ShoppingCart={ShoppingCarts}
+          onDelete={handleDeleteShoppingCart}
+          onSendOrder={handleCreateOrder}
+        />
+      </div>
+
+      <div hidden={!isPlaced}>
+        <Receipt order={order} />
+      </div>
+    </>
+  );
+};
